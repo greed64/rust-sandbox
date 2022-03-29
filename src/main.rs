4e8a13f6e34249236d1254252aea4a8da397a9a4
Rafał <greed640@gmail.com>
2022-03-29 15:17:37 +0200

    mars weight

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+
+    let weight = calculate_weight_on_mars(77.6);
+
+    println!("Weight on Mars: {}kg", weight);
 }
+
+fn calculate_weight_on_mars(weight: f32) -> f32{
+    (weight/ 9.81) * 3.711
+}
+
